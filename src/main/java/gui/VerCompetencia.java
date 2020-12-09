077405e5af861c80150c1264957fb1eb7d64e1e6
Juan Caprin <juankaprin@gmail.com>
2020-12-09 16:00:27 -0300

    generar Fixture listo

--- src/main/java/gui/VerCompetencia.java
+++ src/main/java/gui/VerCompetencia.java
@@ -612,6 +612,7 @@ public class VerCompetencia extends JPanel {
 		JPanel verCompetencia = new VerCompetencia(competenciaDTO, tpPanel, filtros);
 		tpPanel.add(verCompetencia, "VerCompetencia");
 		CardLayout layout = (CardLayout)tpPanel.getLayout();
+		
 		//ver tema usuario
 		if(verComp) {
 			layout.show(tpPanel, "VerCompetencia");
